7f69c641d44701b082a6ee6f968b3490f9e37279
Aswin Andro <aswin.knight@gmail.com>
2026-05-03 14:32:00 +0400

    Update currency-symbols.js

--- currency-symbols.js
+++ currency-symbols.js
@@ -1,6 +1,7 @@
 // currency-symbols.js
 // List of all ISO 4217 currency codes and their symbols
 
+// Reuse the same SVG for all currencies using '$' as their symbol (e.g., USD.svg)
 export const currencySymbols = {
   AED: { ar: 'د.إ', en: 'AED' }, AFN: '؋', ALL: 'L', AMD: '֏', ANG: 'ƒ', AOA: 'Kz', ARS: '$', AUD: '$', AWG: 'ƒ', AZN: '₼',
   BAM: 'KM', BBD: '$', BDT: '৳', BGN: 'лв', BHD: '.د.ب', BIF: 'FBu', BMD: '$', BND: '$', BOB: 'Bs.', BRL: 'R$',
@@ -14,9 +15,9 @@ export const currencySymbols = {
   MNT: '₮', MOP: 'P', MRU: 'UM', MUR: '₨', MVR: 'ރ.', MWK: 'MK', MXN: '$', MYR: 'RM', MZN: 'MT', NAD: '$',
   NGN: '₦', NIO: 'C$', NOK: 'kr', NPR: '₨', NZD: '$', OMR: 'ر.ع.', PAB: 'B/.', PEN: 'S/', PGK: 'K', PHP: '₱',
   PKR: '₨', PLN: 'zł', PYG: '₲', QAR: 'ر.ق', RON: 'lei', RSD: 'дин', RUB: '₽', RWF: 'FRw', SAR: 'ر.س',
-  SBD: '$', SCR: '₨', SDG: 'ج.س.', SEK: 'kr', SGD: '$', SHP: '£', SLE: 'Le', SLL: 'Le', SOS: 'Sh', SRD: '$',
+  SBD: 'USD', SCR: '₨', SDG: 'ج.س.', SEK: 'kr', SGD: 'USD', SHP: '£', SLE: 'Le', SLL: 'Le', SOS: 'Sh', SRD: 'USD',
   SSP: '£', STN: 'Db', SYP: '£', SZL: 'L', THB: '฿', TJS: 'ЅM', TMT: 'm', TND: 'د.ت', TOP: 'T$', TRY: '₺',
-  TTD: '$', TVD: '$', TWD: 'NT$', TZS: 'Sh', UAH: '₴', UGX: 'USh', USD: '$', UYU: '$U', UZS: 'soʻm', VES: 'Bs.',
-  VND: '₫', VUV: 'Vt', WST: 'T', XAF: 'FCFA', XCD: '$', XOF: 'CFA', XPF: '₣', YER: '﷼', ZAR: 'R', ZMW: 'ZK',
-  ZWL: '$'
+  TTD: 'USD', TVD: 'USD', TWD: 'NT$', TZS: 'Sh', UAH: '₴', UGX: 'USh', USD: '$', UYU: '$U', UZS: 'soʻm', VES: 'Bs.',
+  VND: '₫', VUV: 'Vt', WST: 'T', XAF: 'FCFA', XCD: 'USD', XOF: 'CFA', XPF: '₣', YER: '﷼', ZAR: 'R', ZMW: 'ZK',
+  ZWL: 'USD'
 };
